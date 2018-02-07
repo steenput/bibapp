5069b3a8341d5067acc44486e22951fcda736209
steven.liatti <steven.liatti@etu.hesge.ch>
2018-02-07 17:07:09 +0100

    Improve get news, no need of code (information only on Nebis, so not good for future) Close #1
    On branch master
    
    Changes to be committed:
    	new file:   questions.md
    	modified:   wrapper/wrapper.js

---
+++ questions.md
@@ -0,0 +1,2 @@
+# Questions
+Sur cette page (http://hepia.hesge.ch/fr/accueil/bib2/nouveautes/), d'où proviennent les codes pour distinguer les sections ?
--- wrapper/wrapper.js
+++ wrapper/wrapper.js
@@ -13,58 +13,76 @@ const base_url_nebis = 'http://www.library.ethz.ch/rib/v1/primo/';
 const hepia_code = 'E41';
 const lullier_code = 'E67';
 
-app.get('/news/:library_code/:year/:month', function(req, res) {
+let get_library_name = function(c) {
+    let code = c.toUpperCase();
+    if (code === hepia_code) return 'hepia';
+    if (code === lullier_code) return 'lullier';
+}
+
+let get_by_code = function(code, year, month) {
+    return axios.get(base_url_nebis + 'documents', {
+        params: {
+            q: code + year + month,
+            aleph_items: true,
+            lang: 'fr',
+            bulksize: '500',   // maximum authorized by API
+        }
+    });
+};
+
+let compute_documents = function(documents, results, code) {
+    if (results.data.result.hits.totalhits != 0) {
+        results.data.result.document.forEach(d => {
+            let availability = [];
+            d.availability.itemList.forEach(i => {
+                if (i['z30-sub-library-code'] === hepia_code || i['z30-sub-library-code'] === lullier_code) {
+                    availability.push(i);
+                }
+            });
+            let doc = {
+                title: d.biblioData.title,
+                author: d.biblioData.creator,
+                type: d.biblioData.type,
+                identifier: d.biblioData.identifier,
+                creationdate: d.biblioData.creationdate,
+                publisher: d.biblioData.publisher,
+                edition: d.biblioData.edition,
+                description: d.biblioData.description,
+                language: d.biblioData.language,
+                format: d.biblioData.format,
+                keywords: d.biblioData.keywords,
+                availability: availability,
+                library: get_library_name(code)
+            };
+            documents.push(doc);
+        });
+    }
+    return documents;
+};
+
+app.get('/news/:year/:month', function(req, res) {
     log.debug('request url', req.url);
     log.debug('request params', req.params);
     res.type('json');
 
-    const code = req.params.library_code.toUpperCase();
     const year = req.params.year;
     const month = req.params.month;
-    
-    axios.get(base_url_nebis + 'documents', {
-        params: {
-            q: code + year + month,
-            aleph_items: true,
-            sortfield: 'stitle',
-            lang: 'fr',
-            bulksize: '500',   // maximum authorized by API
-        }
-    })
-    .then(news => {
-        log.debug(news.data);
+
+    axios.all([get_by_code(hepia_code, year, month), get_by_code(lullier_code, year, month)])
+    .then(axios.spread(function(hepia, lullier) {
+        log.debug(hepia.data);
+        log.debug(lullier.data);
+
         let documents = [];
-        if (news.data.result.hits.totalhits != 0) {
-            news.data.result.document.forEach(d => {
-                let availability = [];
-                d.availability.itemList.forEach(i => {
-                    if (i['z30-sub-library-code'] === hepia_code || i['z30-sub-library-code'] === lullier_code) {
-                        availability.push(i);
-                    }
-                });
-                let doc = {
-                    title: d.biblioData.title,
-                    author: d.biblioData.creator,
-                    type: d.biblioData.type,
-                    identifier: d.biblioData.identifier,
-                    creationdate: d.biblioData.creationdate,
-                    publisher: d.biblioData.publisher,
-                    edition: d.biblioData.edition,
-                    description: d.biblioData.description,
-                    language: d.biblioData.language,
-                    format: d.biblioData.format,
-                    keywords: d.biblioData.keywords,
-                    availability: availability
-                };
-                documents.push(doc);
-            });
-        }
+        documents = compute_documents(documents, hepia, hepia_code);
+        documents = compute_documents(documents, lullier, lullier_code);
+
         res.status(200).end(JSON.stringify({
             date: new Date(),
             size: documents.length,
             documents: documents
         }));
-    })
+    }))
     .catch(error => {
         log.error('error', error);
         res.status(404).end(JSON.stringify({error: true}));
